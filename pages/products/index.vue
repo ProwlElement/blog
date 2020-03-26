50721273c38d8cd4ba8c05fc27614d6dd2a2e14a
Sean Corcoran <corcoransean28@gmail.com>
2020-03-26 15:06:05 +0000

    styup products and user routes and tested route validate

---
+++ pages/products/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container bg-gray-200">
+    <nuxt-link to="/products/1">Product 1</nuxt-link>
+    <nuxt-link to="/products/2">Product 2</nuxt-link>
+  </div>
+</template>
