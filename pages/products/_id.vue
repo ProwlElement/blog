b59ed69d6449af61f42c3d5c502655b86ccd791f
Sean Corcoran <corcoransean28@gmail.com>
2020-04-17 13:28:22 +0100

    reset project to original generate

--- pages/products/_id.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Single product, with ID: {{ $route.params.id }}</h1>
-</template>
--- pages/products/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container bg-gray-200">
-    <nuxt-link to="/products/1">Product 1</nuxt-link>
-    <nuxt-link to="/products/2">Product 2</nuxt-link>
-  </div>
-</template>
