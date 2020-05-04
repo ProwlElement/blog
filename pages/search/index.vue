550615a69444fc43181a2704782947b02ab228c2
Sean Corcoran <corcoransean28@gmail.com>
2020-05-04 21:38:15 +0100

    started to add elastic search on some test data

---
+++ pages/search/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="about-page flex flex-col justify-center items-center text-center">
+    <section class="search-container py-32">Search Here</section>
+    <section>
+      <h1 class="text-white">Hello from Vendorconex 👋</h1>
+      <ReactiveBase
+        app="partner-search-k4k4hj4kh"
+        credentials="HcGvQDLRN:cd2b5eb2-f976-4497-ba51-b04c6705ce1d"
+      >
+        <div class="filters-container">
+          <!-- <DataSearch
+            componentId="search"
+            data-field="{[
+				    'Category',
+				    'Category.autosuggest',
+				    'Category.keyword'
+				  ]}"
+          />-->
+
+          <MultiList
+            componentId="Authors"
+            data-field="Category.raw"
+            class="filter text-white"
+            title="Select Authors"
+            selectAllLabel="All Authors"
+          />
+        </div>
+
+        <ReactiveList
+          :pagination="true"
+          :from="0"
+          :size="5"
+          :react="{ and: ['Name', 'Category'] }"
+          componentId="results"
+          data-field="Name.raw"
+          paginationAt="bottom"
+          loader="Loading Results.."
+          sortBy="desc"
+          class="text-white"
+        >
+          <div slot="renderItem" slot-scope="{ item }">
+            <div class="flex book-content" key="item._id">
+              <img :src="item.Logo_url" alt="Book Cover" class="book-image" />
+            </div>
+          </div>
+        </ReactiveList>
+      </ReactiveBase>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "search"
+};
+</script>
+
+<style scoped>
+</style>
---
+++ plugins/elastic-search.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+import ReactiveSearch from "@appbaseio/reactivesearch-vue";
+
+Vue.config.productionTip = false;
+Vue.use(ReactiveSearch);
